3be95d0c8e1432394cf0139db9886496a57e5007
ZicanC <chenzican8041@gmail.com>
2024-06-21 16:46:57 -0400

    Update Server.py

--- Server.py
+++ Server.py
@@ -8,8 +8,10 @@ import zlib
 
 # Create a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
+s.bind((socket.gethostbyname(socket.gethostname()), 8485))
 s.listen(5)
+
+print(socket.gethostbyname(socket.gethostname()))
 
 # Initialize the camera
 cam = cv2.VideoCapture(0)
@@ -21,14 +23,15 @@ encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
 
 while True:
     client_socket, address = s.accept()
-    print(f"Connection from {address} has been established!")
+    if client_socket != None:
+        print(f"Connection from {address} has been established!")
 
-    ret, frame = cam.read()
-    result, frame = cv2.imencode('.jpg', frame, encode_param)
-    data = pickle.dumps(frame, 0)
-    size = len(data)
-    print(f"{img_counter}: {size}")
-    client_socket.send(struct.pack(">L", size) + data)
-    img_counter += 1
+        ret, frame = cam.read()
+        result, frame = cv2.imencode('.jpg', frame, encode_param)
+        data = pickle.dumps(frame, 0)
+        size = len(data)
+        print(f"{img_counter}: {size}")
+        client_socket.send(struct.pack(">L", size) + data)
+        img_counter += 1
 
 cam.release()
